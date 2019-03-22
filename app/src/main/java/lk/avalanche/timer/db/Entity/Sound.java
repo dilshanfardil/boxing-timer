2cc582183483eff63f96ef3514bfdd49ca3c0dc9
Lahiru <ahlahiru.1@gmail.com>
2019-03-22 11:39:11 +0530

    new fragment

---
+++ app/src/main/java/lk/avalanche/timer/db/Entity/Sound.java
@@ -0,0 +1,51 @@
+package lk.avalanche.timer.db.Entity;
+
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+/*
+ * Developed by Lahiru Muthumal on 3/22/2019.
+ * Last modified $file.lastModified.
+ *
+ * (C) Copyright 2019.year avalanche.lk (Pvt) Limited.
+ * All Rights Reserved.
+ *
+ * These materials are unpublished, proprietary, confidential source code of
+ * avalanche.lk (Pvt) Limited and constitute a TRADE SECRET
+ * of avalanche.lk (Pvt) Limited.
+ *
+ * avalanche.lk (Pvt) Limited retains all title to and intellectual
+ * property rights in these materials.
+ *
+ */
+@Entity(tableName = "sound")
+public class Sound {
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+    private String name;
+    private boolean type;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isType() {
+        return type;
+    }
+
+    public void setType(boolean type) {
+        this.type = type;
+    }
+}
